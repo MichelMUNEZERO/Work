b91f4394b5901e97db1aa9877ff52358758903e0
mukamfizi gisele <giselmukamfizi@gmail.com>
2024-11-26 11:53:48 +0200

    Question1 completed

---
+++ finalproject/src/Components/1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Welcome(){
+    return <p>Welcome to my student project</p>;
+}
+
+function CurrentDate(){
+    return <p>The date is {Date()}</p>;
+}
+export {Welcome, CurrentDate};
